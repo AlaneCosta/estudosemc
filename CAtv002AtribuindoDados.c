6e93a064718724faa129417a1b1876ca40102838
Alane Costa <134459619+AlaneCosta@users.noreply.github.com>
2023-11-30 17:57:12 -0300

    Aprendendo variáveis e atribuição de dados

---
+++ CAtv002AtribuindoDados.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define texto "Entrada e saída de dados."
+
+
+int main() {
+	
+	printf("%s\n", texto);
+	
+	int idade = 0;
+	float altura = 0.0;
+	char nome[50] = "";
+	
+	printf("Digite a idade:\n");
+	scanf("%d", &idade);
+	
+	printf("Digite a altura:\n");
+	scanf("%f", &altura);
+	
+	printf("Digite o nome:\n");
+	scanf("%s", nome);	
+	
+	printf("\nDados Informados:\n");
+	printf("Idade: %d \n", idade);
+	printf("Altura: %.2f\n", altura);
+	printf("Nome: %s\n", nome);
+	return 0;
+}
