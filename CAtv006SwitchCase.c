ec5cd507358be2bf64696daddfcbbf77e002a2ef
Alane Costa <134459619+AlaneCosta@users.noreply.github.com>
2023-12-01 16:55:28 -0300

    Utilizando SWITCH-CASE

---
+++ CAtv006SwitchCase.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL,"");
+	
+	int d;
+	
+	printf("Insira um valor de 1 a 7:\n");
+	scanf("%d", &d);
+	
+	switch(d){
+		case 1:
+			printf("Domingo.\n");
+			break;
+		case 2:
+			printf("Segunda-feira.\n");
+			break;			
+		case 3:
+			printf("Terça-feira.\n");
+			break;	
+		case 4:
+			printf("Quarta-feira.\n");
+			break;					
+		case 5:
+			printf("Quinta-feira.\n");
+			break;
+		case 6:
+			printf("Sexta-feira.\n");
+			break;			
+		case 7:
+			printf("Sábado.\n");
+			break;
+		default:
+			printf("Valor Inválido.\n");
+			break;								
+	}
+	
+	return 0;
+}
