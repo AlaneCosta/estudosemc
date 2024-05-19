6593269c7a461048aac8b41c4d305c7de961ae6e
alanecosta <alanecosta@gmail.com>
2024-05-19 12:55:38 -0300

    alocação dinâmica de vetor

--- EstruturaDados/alocacaodinamica.c
+++ EstruturaDados/alocacaodinamica.c
@@ -3,7 +3,10 @@
 
 int main() {
   int *p;
+  int *vetor;
   p = (int *) malloc (sizeof(int));
+  vetor = (int *) malloc (sizeof(int)*10);
+
   if (p == NULL){
     printf("Erro!\n");
   }          
@@ -11,5 +14,14 @@ int main() {
     *p = 10;
     printf("p: %d\n", *p);
     free(p);
+  }  
+
+  if (vetor == NULL){
+    printf("Erro!\n");
+  }          
+  else {
+    *vetor = 60;
+    printf("p: %d\n", *vetor);
+    free(vetor);  
   }
 }          
